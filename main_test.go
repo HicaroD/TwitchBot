264939eb86ef9fff051b08e87edc2850eebc5f44
HicaroD <hdanrlley1@gmail.com>
2022-03-17 19:49:34 -0300

    Adding tests for `send_command` when input is empty

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func test_if_gets_error_when_command_is_empty(t *testing.T) {
+	irc, _ := new_irc()
+	inputs := [][]string{{"", "some data"}, {"some data", ""}, {"", ""}}
+
+	for _, input := range inputs {
+		err := irc.send_command(input[0], input[1])
+		if err != nil {
+			t.Errorf("It shouldn't be empty!")
+		}
+	}
+}
