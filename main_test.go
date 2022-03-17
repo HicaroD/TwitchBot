88b98dd92440f087043498f9fd50a7361edf9c5c
HicaroD <hdanrlley1@gmail.com>
2022-03-17 19:55:15 -0300

    Adding `send_message` to same test
    I don't really know if I want to create a new test method for the same
    code structure when I can just add the method to the the same test

--- main_test.go
+++ main_test.go
@@ -3,11 +3,17 @@ package main
 import "testing"
 
 func test_if_gets_error_when_command_is_empty(t *testing.T) {
+    var err error
 	irc, _ := new_irc()
 	inputs := [][]string{{"", "some data"}, {"some data", ""}, {"", ""}}
 
 	for _, input := range inputs {
-		err := irc.send_command(input[0], input[1])
+		err = irc.send_command(input[0], input[1])
+		if err != nil {
+			t.Errorf("It shouldn't be empty!")
+		}
+
+		err = irc.send_message(input[0], input[1])
 		if err != nil {
 			t.Errorf("It shouldn't be empty!")
 		}
