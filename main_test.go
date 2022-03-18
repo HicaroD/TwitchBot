0838f7d4ee6486f74f47051a7697db51f095661c
HicaroD <hdanrlley1@gmail.com>
2022-03-18 14:28:38 -0300

    Test: Fixing test bug because it should check if `err is nil`
    The inputs are intentionally wrong because I want to see if the program
    detects the errors, therefore I should check `if err == nil`

--- main_test.go
+++ main_test.go
@@ -2,20 +2,20 @@ package main
 
 import "testing"
 
-func test_if_gets_error_when_command_is_empty(t *testing.T) {
+func Test_if_gets_error_when_command_is_empty(t *testing.T) {
 	var err error
 	irc, _ := new_irc()
 	inputs := [][]string{{"", "some data"}, {"some data", ""}, {"", ""}}
 
 	for _, input := range inputs {
 		err = irc.send_command(input[0], input[1])
-		if err != nil {
-			t.Errorf("It shouldn't be empty!")
+		if err == nil {
+			t.Fatalf("Command or body shouldn't be empty!")
 		}
 
 		err = irc.send_message(input[0], input[1])
-		if err != nil {
-			t.Errorf("It shouldn't be empty!")
+		if err == nil {
+			t.Fatalf("Message shouldn't be empty!")
 		}
 	}
 }
